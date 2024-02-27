dcaac0f9b2587a3220bddfd51070e3002a50c8df
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-02-27 13:08:23 +0530

    added the time bar and hud functionalities

--- VanRakshak.cpp
+++ VanRakshak.cpp
@@ -1,6 +1,6 @@
 
 // Our Game Starts from here
-
+#include <sstream>
 #include <iostream>
 #include<SFML/Graphics.hpp>
 using namespace sf;
@@ -67,6 +67,47 @@ int main()
 
     // Variables to control time itself
     Clock clock;
+    // Time bar
+    RectangleShape timeBar;
+    float timeBarStartWidth = 400;
+    float timeBarHeight = 80;
+    timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+    timeBar.setFillColor(Color::Red);
+    timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+    Time gameTimeTotal;
+    float timeRemaining = 6.0f;
+    float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+    // Track whether the game is running
+    bool paused = true;
+
+    // Draw some text
+    int score = 0;
+    Text messageText;
+    Text scoreText;
+    // We need to choose a font
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+    // Set the font to our message
+    messageText.setFont(font);
+    scoreText.setFont(font);
+    // Assign the actual message
+    messageText.setString("Press Enter to start!");
+    scoreText.setString("Score = 0");
+    // Make it really big
+    messageText.setCharacterSize(75);
+    scoreText.setCharacterSize(100);
+    // Choose a color
+    messageText.setFillColor(Color::White);
+    scoreText.setFillColor(Color::White);
+
+    // Position the text
+    FloatRect textRect = messageText.getLocalBounds();
+    messageText.setOrigin(textRect.left +
+        textRect.width / 2.0f,
+        textRect.top +
+        textRect.height / 2.0f);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+    scoreText.setPosition(20, 20);
 
     while (window.isOpen())
     {
@@ -79,14 +120,45 @@ int main()
         {
             window.close();
         }
+        // Start the game
+        if (Keyboard::isKeyPressed(Keyboard::Return))
+        {
+            paused = false;
+        }
+        // Reset the time and the score
+        score = 0;
+        timeRemaining = 6;
+
         /*
         ****************************************
         Update the scene
         ****************************************
         */
-
+        if (!paused)
+        {
         // Measure time
         Time dt = clock.restart();
+
+        // Subtract from the amount of time remaining
+        timeRemaining -= dt.asSeconds();
+        // size up the time bar
+        timeBar.setSize(Vector2f(timeBarWidthPerSecond *
+            timeRemaining, timeBarHeight));
+
+        if (timeRemaining <= 0.0f) {
+
+            // Pause the game
+            paused = true;
+            // Change the message shown to the player
+            messageText.setString("Out of time!!");
+            //Reposition the text based on its new size
+            FloatRect textRect = messageText.getLocalBounds();
+            messageText.setOrigin(textRect.left +
+                textRect.width / 2.0f,
+                textRect.top +
+                textRect.height / 2.0f);
+            messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+        }
 
         // Setup the bee
         if (!beeActive)
@@ -190,6 +262,14 @@ int main()
             }
         }
 
+        // Update the score text
+        std::stringstream ss;
+        ss << "Score = " << score;
+        scoreText.setString(ss.str());
+
+        } // End if(!paused)
+
+
         /*
         ****************************************
         Draw the scene
@@ -208,6 +288,16 @@ int main()
         // Draw the insect
         window.draw(spriteBee);
         // Show everything we just drew
+        // Draw the score
+        window.draw(scoreText);
+        // Draw the timebar
+        window.draw(timeBar);
+
+        if (paused)
+        {
+            // Draw our message
+            window.draw(messageText);
+        }
         window.display();
     }
     std::cout << "Hello World!\n";
